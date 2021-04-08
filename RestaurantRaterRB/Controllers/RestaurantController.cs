92f717f99451eced49d03d4b38dd499cfceb5020
michael Davidson <meridianproservice@outlook.com>
2021-04-07 20:37:58 -0400

    Edit/controll and view

--- RestaurantRaterRB/Controllers/RestaurantController.cs
+++ RestaurantRaterRB/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterRB.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -71,22 +72,33 @@ namespace RestaurantRaterRB.Controllers
         // Find a Restaurant by thae id
         // If the restaurant doesnt exist
         // Resturn the restaurant and the view
-
         public ActionResult Edit(int? id)
         {
             if(id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            return View();
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
 
         }
 
         // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
-
-
-
-
 }
